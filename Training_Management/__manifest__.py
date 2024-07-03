abb641f9307e603106eba8f969dd17aae0ea30e1
uid1160 <soumaya.douggi@focus-corporation.com>
2024-07-03 14:54:11 +0100

    [ FIX ]

--- Training_Management/__manifest__.py
+++ Training_Management/__manifest__.py
@@ -28,7 +28,7 @@
 
         ],
     },
-    'images':['Training_Management/static/description/icon.png'],
+    'images':['/static/description/icon.png'],
     'demo': [],
     'qweb': [],
     'installable': True,
